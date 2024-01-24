0d68053882c1890cbe4266f1722adc185c3e3f4e
anowar24 <anowarhu024@gmail.com>
2024-01-24 22:45:03 +0530

    header added

---
+++ src/components/common/header.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function header() {
+  return (
+    <main className="flex h-32 bg-blue-200 flex-col items-center justify-between p-24">
+     <div className="text-red-500 text-3xl">Club Space</div>
+    </main>
+  );
+}
