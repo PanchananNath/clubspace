55d3712833c2e0595fea996afcf61528b94e4847
anowar24 <anowarhu024@gmail.com>
2024-01-24 22:34:21 +0530

    init

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function LoginPAGE() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <div className="text-red-500 text-3xl">lOGIN pAGE</div>
+    </main>
+  );
+}
