0e3b95ca780d50f571c03252041fb93dbe1398a0
Debanand Singha <debanand1101@gmail.com>
2024-02-02 22:29:01 +0530

    Create SideBar Day-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
+import SideBar from "@/components/common/SideBar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div className="text-red-500 text-3xl">Club Space</div>
-     <Link href="/login" className="font-bold text-5xl text-blue-500">Login</Link>
+    <main className="flex">
+      <SideBar />
     </main>
   );
 }
---
+++ src/components/common/SideBar.tsx
@@ -0,0 +1,63 @@
+import { LuLayoutDashboard } from "react-icons/lu";
+import { MdEvent } from "react-icons/md";
+import { PiUsersThreeFill } from "react-icons/pi";
+import { MdAccountCircle } from "react-icons/md";
+import { FiSettings } from "react-icons/fi";
+import { TbLogout } from "react-icons/tb";
+
+export default function SideBar() {
+  return (
+    <aside className="h-screen">
+      <nav className="h-full flex flex-col bg-primary">
+        <div className="p-4 mb-10 flex items-center text-white font-extrabold text-2xl">
+          <img
+            src="https://logos-download.com/wp-content/uploads/2016/02/BMW_logo_big_transparent_png-700x700.png"
+            alt="L"
+            className="h-10 w-10"
+          />
+          <span className="ml-3">Club Space</span>
+        </div>
+        <ul className="flex-1">
+          <SideBarItem
+            icon={<LuLayoutDashboard className="text-white h-7 w-7" />}
+            text={"Dashboard"}
+            active={true}
+          />
+          <SideBarItem
+            icon={<MdEvent className="text-white h-7 w-7" />}
+            text={"Events"}
+          />
+          <SideBarItem
+            icon={<PiUsersThreeFill className="text-white h-7 w-7" />}
+            text={"All Clubs"}
+          />
+          <SideBarItem
+            icon={<MdAccountCircle className="text-white h-7 w-7" />}
+            text={"My Account"}
+          />
+        </ul>
+        <ul className="flex-0">
+          <SideBarItem
+            icon={<FiSettings className="text-white h-7 w-7" />}
+            text={"Settings"}
+          />
+          <SideBarItem
+            icon={<TbLogout className="text-white h-7 w-7" />}
+            text={"Log Out"}
+          />
+        </ul>
+      </nav>
+    </aside>
+  );
+}
+
+function SideBarItem(props: { icon: any; text: String; active?: boolean }) {
+  return (
+    <li className="m-3 p-3 h-fill w-fill ">
+      <a href="" className="flex items-center">
+        {props.icon}
+        <span className="ml-4 text-white">{props.text}</span>
+      </a>
+    </li>
+  );
+}
