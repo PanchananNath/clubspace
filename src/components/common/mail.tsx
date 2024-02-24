a1fd341eaf6c82e88de5d6d821665dedd2e7bfdd
Monalisha Roy <118293636+Monalisha-Roy@users.noreply.github.com>
2024-02-24 19:21:09 +0530

    Update mail.tsx

--- src/components/common/mail.tsx
+++ src/components/common/mail.tsx
@@ -4,7 +4,7 @@ export default function Mail() {
     return(<div className="bg-blue-50 text-black">
         <h1 className="text-bold">Redirect to Mail in device!</h1>
         <h2>This is just a temporary interface which will actually be the mail in the users inbox.</h2>
-        <Link href="./createpassword"><button className="bg-[#365486] rounded-lg text-white py-2 px-4 hover:scale-105 duration-300">Create new Password</button></Link>
+        <button className="bg-[#365486] rounded-lg text-white py-2 px-4 hover:scale-105 duration-300"><Link href="./createpassword">Create new Password</Link></button>
     </div>   
     )
 }
