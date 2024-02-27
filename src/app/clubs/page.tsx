17c2f9d445cb02e688831cbbc867fef0c08fe5fd
Anowar Hussain Mondal <53121515+anowar24@users.noreply.github.com>
2024-02-27 18:14:48 +0000

    card comp

--- src/app/clubs/page.tsx
+++ src/app/clubs/page.tsx
@@ -1,8 +1,36 @@
-export default function Clubs() {
+import CustomCard, { CardProps } from "@/components/clubcard";
+import { sql } from "@vercel/postgres";
+
+
+
+
+export default async function Clubs() {
+  
+const { rows } = await sql`SELECT id,name,about FROM clubs;`;
+
+console.log(rows);
+
   return (
     <main className="flex min-h-screen w-full items-center justify-center p-1">
-      <div className="bg-secondary h-full w-full rounded-2xl flex justify-center">
-        <div className="self-center text-2xl font-bold">Clubs</div>
+      <div className="bg-secondary min-h-full w-full rounded-2xl p-20">
+     <div className="bg-slate-100  py-20 px-44">
+      <div>Clubs</div>
+      
+     
+          {rows.map((item ) => {
+            console.log(item);
+            return (
+              <div className="w-64" key={item.id}>
+              <CustomCard title={item.name} description={item.about} icon={""} />
+              </div>
+            )}
+
+        )
+      }
+
+   
+        </div>
+
       </div>
     </main>
   );
