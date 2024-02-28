ea68cb777fdcb88b4ea7926b9f3da0a4641475ba
Jharendrajogi <jharendrajogi123@gmail.com>
2024-02-28 20:11:49 +0530

    myremainder page completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import MyRemainders from "@/components/common/myRemainder/myRemainder";
+import NavBar from "@/components/common/navBar";
+import MyClubs from "@/components/myRemainder/myRemainder";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <div className="text-red-500 text-3xl">Club Space</div>
+     <MyRemainders/>
     </main>
   );
 }
