74775bb654ee1f64d3dded5123fb441b7e9389d6
Debanand Singha <debanand1101@gmail.com>
2024-03-09 02:13:01 +0530

    Event Component update

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -4,9 +4,9 @@ import { BiCommentDetail } from "react-icons/bi";
 import Image from "next/image";
 import LikeButton from "./likeButton";
 
-export default function Events1(){
+export default function Events(){
   return(
-     <div className="h-screen w-full bg-white relative overflow-y-scroll  ">
+     <div className="h-screen w-full bg-white relative overflow-y-scroll rounded-2xl ">
 
       <div className="h-content w-full bg- p-5">
             <div className="flex justify-end items-center gap-3">
@@ -32,9 +32,9 @@ export default function Events1(){
                           alt="logo"
                           height={40}
                           width={40}
-                          // className="rounded-full"  
+                          className="mr-3"  
                       />
-                      <h1>Cyber X Secure Club</h1>
+                      <h1 className="font-bold">Cyber X Secure Club</h1>
                   </div>
                   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit dolore sequi, eius reprehenderit maxime perferendis architecto veritatis distinctio blanditiis. Dolores tempora, rerum mollitia iste quod voluptates exercitationem aperiam ipsa odio, sunt, nesciunt pariatur hic! Possimus molestias error sit eveniet vero inventore iusto hic molestiae odit. Quae quod iste autem facilis. </p>
                   <div className="flex justify-center items-center mb-5 mt-5">
@@ -86,9 +86,9 @@ export default function Events1(){
                           alt="logo"
                           height={40}
                           width={40}
-                          // className="rounded-full"  
+                          className="mr-3"  
                       />
-                      <h1>AI Club</h1>
+                      <h1 className="font-bold">AI Club</h1>
                   </div>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa non, dolor soluta similique ut animi exercitationem sequi natus repellat quam doloremque enim. Sequi fugiat aperiam eveniet quod repellat aut ducimus iusto, iste praesentium tempore necessitatibus magnam esse maxime labore quam asperiores quaerat! Doloremque debitis deserunt impedit ut harum eligendi in, aliquid nulla amet at quaerat assumenda error eum explicabo corrupti ea voluptas, sunt, quo veniam. Soluta quos repellat ducimus perspiciatis saepe fugiat doloremque hic dolore exercitationem optio? Sequi id, voluptates nobis tempora dignissimos doloremque, illo minus omnis aut optio quis vitae perferendis adipisci quas itaque? Cumque vitae neque nulla ducimus saepe perferendis quam temporibus, provident itaque voluptates harum necessitatibus consequuntur quaerat reprehenderit fuga voluptatibus reiciendis animi odit natus laborum quibusdam optio ab! Similique accusantium nihil blanditiis vel ad. Dolore impedit enim accusantium quos dicta! Perspiciatis, velit vel dignissimos excepturi atque quisquam temporibus ipsa architecto accusamus fuga consequuntur quos voluptas ratione. Sit, modi est ut doloribus expedita eos illo libero placeat nemo obcaecati. Sed facere eos dolores necessitatibus eveniet excepturi officiis, perspiciatis repellendus cum culpa provident cupiditate ab. Quae, aut. Assumenda voluptatum fuga ea qui, doloremque, facilis, modi voluptatibus vel nemo culpa autem labore dignissimos? Fuga tenetur dolores atque reprehenderit magni.</p>
                   <div className="flex justify-center items-center mb-5 mt-5">
