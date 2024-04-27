aaaf1902b0996bbe4a67483738131cbf230d2e53
RahulBardhan <rahulbardhan285@gmail.com>
2024-04-27 22:35:37 +0530

    Commited on 27-04-24

--- src/components/allclubs/fetchdata.tsx
+++ src/components/allclubs/fetchdata.tsx
@@ -1,34 +1,24 @@
 import React from "react";
 
 export interface CardProps {
-    id : string;
-    name : string;
-    logo : string;
-    website_url : string;
-    catagory : string;
-    advisor : string;
-    convener : string;
-    description : string;
-    contact_info : string;
-    social_media_links : string;
+  id: number;
+  name: string;
+  about: string;
+  vision: string;
+  objective: string;
+  poster: string;
 }
-const CustomCard: React.FC<CardProps> = ({ id, name, logo, website_url, catagory, advisor, convener, description, contact_info, social_media_links }) => {
+const CustomCard: React.FC<CardProps> = ({id, name, about, vision, objective, poster}) => {
   return (
-    
     <div className="bg-white shadow-md sm:p-5 p-1 sm:m-2 m-1 text-black  self-stretch flex flex-col items-start justify-start text-center">
 
 
-      <div>Id : {id}</div>
-      <div>Name : {name}</div>
-      <div>Logo : {logo}</div>
-      <div>Website_url : {website_url}</div>
-      <div>Catagory : {catagory}</div>
-      <div>Advisor : {advisor}</div>
-      <div>onvener: {convener}</div>
-      <div>Description : {description}</div>
-      <div>Contact_info : {contact_info}</div>
-      <div>Social_media_links : {social_media_links}</div>
-      
+        <div>Id: {id}</div>
+        <div>Name: {name}</div>
+        <div>about: {about}</div>
+        <div>Vision: {vision}</div>
+        <div>Objective: {objective}</div>
+        <div>Poster: {poster}</div>
     </div>
   );
 };
